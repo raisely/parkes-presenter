6b6dd2b0281dcd9b8b45bfcb64a8a4cbeeeb7d15
Chris Jensen <chris@broadthought.co>
2017-11-03 09:51:37 +0800

    Initial draft of all tests

--- test/jsonPresenter.test.js
+++ test/jsonPresenter.test.js
@@ -1,64 +1,131 @@
-const presenters = require('../');
+const Presenter = require('../');
 const testDb = require('./testModels');
-const { expect } = require('chai');
+const chai = require('chai');
+const { chaiSubset } = require('chai-subset');
+const _ = require('lodash');
 
 const { models } = testDb;
+const { expect } = chai;
 
-function checkForeignKeys() {
-	it('populates public foreign keys', () => {
-
-	});
-	it('populates nested array models', () => {
-		expect(result.comments[0])
-	});
-	it('populates nested model', () => {
-		expect(result.comments)
-	});
-	it('renames nested models', () => {
-
-	});
-}
+chai.use(chaiSubset);
 
 describe('jsonPresenter', () => {
-	before(async () => {
-		// Extend all models with the presenters
-		models.forEach((model) => {
-			Object.assign(model.prototype, presenters);
-		});
+	let user;
+	let json;
+	let presentationKey = 'uuid';
+	const expectedUser = { name: 'Bill Ryan', role: 'Veteran' };
 
+	before(async () => {
+		Presenter.extendModels(models);
 		await testDb.init();
 	});
 
-	describe('present', () => {
-		it('copies in attributes', () => {
+	beforeEach(async () => {
+		user = await models.user.findOne();
+	});
 
+	describe('present', () => {
+		beforeEach(() => {
+			json = user.present(models.user.publicAttributes, {
+				include: models.user.constructor.nestedModels,
+			});
 		});
+
+		it('copies in attributes', () => {
+			expect(json).to.containSubset(expectedUser);
+		});
+
+		it('defines null attributes', () => {
+			expect(json).to.have.all.keys('bio');
+		});
+
+		function checkForeignKeys() {
+			it('populates public foreign keys', () => {
+				const key = `team${_.capitalize(presentationKey)}`;
+				expect(user[key]).to.equal(`${presentationKey}_for_team`);
+			});
+			it('populates renamed nested models', () => {
+				const key = `author${_.capitalize(presentationKey)}`;
+				expect(user.posts[0][key]).to.equal(`${presentationKey}_for_user`);
+			});
+		}
+
+		context('nested models', () => {
+			it('includes nested model', () => {
+				expect(json.team).to.containSubset({ name: 'Generations' });
+			});
+
+			it('includes nested collection', () => {
+				expect(json.posts).to.containSubset([{ title: 'The Defining Moment of our Times' }]);
+			});
+
+			it('renames nested models', async () => {
+				const post = await models.user.findOne();
+				json = post.toPublic();
+				expect(json.author).to.containSubset(expectedUser);
+			});
+		});
+
 		context('default public key', () => {
 			checkForeignKeys();
 		});
 		context('public key is key', () => {
 			beforeEach(() => {
 				models.forEach((model) => { model.presentationKey = 'key'; });
+				presentationKey = 'key';
 			});
 			afterEach(() => {
 				// Reset after the tests
 				models.forEach((model) => { model.presentationKey = null; });
+				presentationKey = 'uuid';
 			});
 
 			checkForeignKeys();
 		});
 	});
-	describe('toPublic', () => {
-		it('presents public attributes', () => {
 
+	function itPresentsPublicAttributes() {
+		it('presents public attributes', () => {
+			const expected = { name: 'Bill Ryan', role: 'Veteran' };
+			expect(json).toMatchObject(expected);
+		});
+	}
+
+	describe('toPublic', () => {
+		beforeEach(() => {
+			json = user.toPublic();
+		});
+
+		itPresentsPublicAttributes();
+
+		it('does not include private attributes', () => {
+			expect(json).to.not.have.all.keys('role');
 		});
 		it('calls correct presenter in nested models', () => {
+			expect(user.team.secretPower).to.not.have.all.keys(models.team.privateAttributes);
+			expect(user.posts[0]).to.not.have.all.keys(models.post.privateAttributes);
+		});
+		it("doesn't include private models", () => {
 
 		});
 	});
 	describe('toPrivate', () => {
-		it('presents private attributes', () => {
+		beforeEach(() => {
+			json = user.toPrivate();
+		});
 
+		itPresentsPublicAttributes();
+
+		it('presents private attributes', () => {
+			const expected = { role: 'Veteran' };
+			expect(json).toMatchObject(expected);
+		});
+		it('calls correct presenter in nested models', () => {
+			expect(user.team.secretPower).to.have.all.keys(models.team.privateAttributes);
+			expect(user.posts[0]).to.have.all.keys(models.post.privateAttributes);
+		});
+		it('includes private models', () => {
+			expect(user.comments[0].to.have.all.keys(models.comments.privateAttributes));
 		});
 	});
 });
--- test/testModels.js
+++ test/testModels.js
@@ -1,73 +1,138 @@
 const Sequelize = require('sequelize');
 
-const sequelize = function testDB() {
-	return new Sequelize('parkesPresenterTest', 'parkes-test', 'parkes-password', {
-		dialect: 'sqlite',
+function defineUser(sequelize) {
+	sequelize.define('user', {
+		uuid: Sequelize.STRING,
+		key: Sequelize.STRING,
+		name: Sequelize.STRING,
+		role: Sequelize.STRING,
+		bio: Sequelize.STRING,
 	});
-};
+	Object.assign(sequelize.models.user, {
+		publicAttributes: ['uuid', 'key', 'name', 'bio'],
+		privateAttributes: ['role'],
+		nestedModels: ['posts', 'team'],
+	});
+	sequelize.models.user.associate = (m) => {
+		m.user.belongsTo(m.team);
+		m.user.hasMany(m.post);
+		m.user.hasMany(m.comment);
+	};
+}
 
-sequelize.define('user', {
-	uuid: Sequelize.STRING,
-	key: Sequelize.STRING,
-	name: Sequelize.STRING,
-	role: Sequelize.STRING,
-});
-Object.assign(sequelize.models.user, {
-	publicAttributes: ['uuid', 'key', 'name'],
-	privateAttributes: ['role'],
-	nestedModels: ['posts', 'team'],
-});
+function definePost(sequelize) {
+	sequelize.define('post', {
+		uuid: Sequelize.STRING,
+		key: Sequelize.STRING,
+		title: Sequelize.STRING,
+		body: Sequelize.STRING,
+		followers: Sequelize.INTEGER,
+	});
+	Object.assign(sequelize.models.post, {
+		publicAttributes: ['uuid', 'key', 'title', 'body'],
+		privateAttributes: ['follows'],
+		nestedModels: [{ attribute: 'user', rename: 'author' }],
+	});
+	sequelize.models.post.associate = (m) => {
+		m.post.belongsTo(m.user);
+	};
+}
 
-sequelize.define('post', {
-	uuid: Sequelize.STRING,
-	key: Sequelize.STRING,
-	title: Sequelize.STRING,
-	body: Sequelize.STRING,
-	followers: Sequelize.INTEGER,
-});
-Object.assign(sequelize.models.post, {
-	publicAttributes: ['uuid', 'key', 'title', 'body'],
-	privateAttributes: ['follows'],
-	nestedModels: [{ attribute: 'user', rename: 'author' }],
-});
+function defineTeam(sequelize) {
+	sequelize.define('team', {
+		uuid: Sequelize.STRING,
+		key: Sequelize.STRING,
+		name: Sequelize.STRING,
+		secretPower: Sequelize.STRING,
+	});
+	Object.assign(sequelize.models.team, {
+		publicAttributes: ['uuid', 'key', 'name'],
+		privateAttributes: ['secretPower'],
+		nestedModels: ['user'],
+	});
+	sequelize.models.team.associate = (m) => {
+		m.team.hasMany(m.user);
+	};
+}
 
-sequelize.define('team', {
-	uuid: Sequelize.STRING,
-	key: Sequelize.STRING,
-	name: Sequelize.STRING,
-	secretPower: Sequelize.STRING,
-});
-Object.assign(sequelize.models.team, {
-	publicAttributes: ['uuid', 'key', 'name'],
-	privateAttributes: ['secretPower'],
-	nestedModels: ['user'],
-});
+function defineComment(sequelize) {
+	sequelize.define('comment', {
+		uuid: Sequelize.STRING,
+		key: Sequelize.STRING,
+		body: Sequelize.STRING,
+	});
+	Object.assign(sequelize.models.comment, {
+		publicAttributes: ['uuid', 'key', 'name'],
+		nestedModels: {
+			public: ['post'],
+			private: ['user'],
+		},
+	});
+	sequelize.models.comment.associate = (m) => {
+		m.comment.belongsTo(m.user);
+		m.comment.belongsTo(m.post);
+	};
+}
 
-async function init() {
-	await sequelize.sync({ force: true });
+function configureDb() {
+	const sequelize = new Sequelize('parkesPresenterTest', 'parkes-test', 'parkes-password', {
+		dialect: 'sqlite',
+		operatorsAliases: false,
+	});
 
-	// This can all happen in parallel
-	await sequelize.models.post.create({
-		title: 'The Defining Moment of our Times',
-		followers: 11000,
-		uuid: 'uuid_for_post',
-		key: 'key_for_post',
-		user: {
+	defineUser(sequelize);
+	definePost(sequelize);
+	defineTeam(sequelize);
+	defineComment(sequelize);
+
+	Object.keys(sequelize.models).forEach((name) => {
+		if (sequelize.models[name].associate) {
+			sequelize.models[name].associate(sequelize.models);
+		}
+	});
+
+	return sequelize;
+}
+
+function defineInit(sequelize) {
+	return async function init() {
+		await sequelize.sync({ force: true });
+
+		const team = await sequelize.models.team.create({
+			name: 'Generations',
+			secretPower: 'people',
+			uuid: 'uuid_for_team',
+			key: 'key_for_team',
+		});
+
+		const user = await sequelize.models.user.create({
 			name: 'Bill Ryan',
 			role: 'Veteran',
 			uuid: 'uuid_for_bill',
 			key: 'key_for_bill',
-			team: {
-				name: 'Generations',
-				secretPower: 'people',
-				uuid: 'uuid_for_team',
-				key: 'key_for_team',
-			},
-		},
-	});
+			teamId: team.id,
+		});
+
+		await sequelize.models.post.create({
+			title: 'The Defining Moment of our Times',
+			followers: 11000,
+			uuid: 'uuid_for_post',
+			key: 'key_for_post',
+			userId: user.id,
+		});
+
+		await sequelize.models.comments.create({
+			uuid: 'uuid_for_comment',
+			key: 'key_for_comment',
+			body: 'I say, I say, I say',
+		});
+	};
 }
 
+const sequelize = configureDb();
+
 module.exports = {
-	init,
+	init: defineInit(sequelize),
+	sequelize,
 	models: sequelize.models,
 };
